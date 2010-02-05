54be893d639f99e24d4b87f483566eadac0e7913
asolove <asolove@gmail.com>
2010-02-04 20:35:03 -0500

    Fixes for user accounts

--- src/server.js
+++ src/server.js
@@ -149,8 +149,8 @@ new User("kpowers", "alphabeta3", kpowers_room.id);
 new User("kpowers_guest", "betaboost8", kpowers_room.id);
 
 var markf_room = GLOBAL.markf_room = new Room([]);
-new User("markf", "betatastic2"); // mark
-new User("markf_guest", "betaboost0");
+new User("markf", "betatastic2", markf_room.id); // mark
+new User("markf_guest", "betaboost0", markf_room.id);
 
 var daltonlp_room = GLOBAL.daltonlp_room = new Room([]);
 new User("daltonlp", "betafish4", daltonlp_room.id); // daltonlp@gmail.com
@@ -190,7 +190,7 @@ function withSession(req, res){
 }
 
 var join_request = function(req, res){
-  var params = qs.parse(url.parse(req.url).query),
+  var params = qs.parse(url.parse(req.url).query || ""),
       username = params.username, password = params.password,
       user = User.find(username, password);
   if(!user){
